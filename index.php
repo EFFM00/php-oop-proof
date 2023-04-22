5c99b8ddf3417ade548b9eedf65aa2c4c096af42
EFFM00 <fischietto.elena@gmail.com>
2023-04-22 17:19:34 -0300

    learning getters and setters in PHP

--- index.php
+++ index.php
@@ -2,3 +2,28 @@
 
 require 'vendor/autoload.php';
 
+class Post{
+
+    private string $id;
+
+    public function saludo() {
+        return "Hola";
+    }
+
+    public function getId():string {
+        return $this->id;
+    }
+
+    public function setId(string $id) {
+        $this->$id = $id;
+    }
+
+
+}
+
+$obj = new Post;
+
+// var_dump($obj);
+
+echo $obj->getId();
+echo $obj->saludo();
