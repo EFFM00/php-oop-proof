4fa9cba9112c1ff54a334546d0fb49c2cfaa6602
EFFM00 <fischietto.elena@gmail.com>
2023-04-22 17:37:32 -0300

    construct and new functionalities php 8

--- index.php
+++ index.php
@@ -5,6 +5,19 @@ require 'vendor/autoload.php';
 class Post{
 
     private string $id;
+    private string $message;
+
+
+    // Se pueden ahorrar líneas de código escribiendo el alcance de la variable en el constructor, y se evita declarar la propiedad más arriba.
+    // public function __construct(private string $message) {
+    public function __construct(string $message) {
+        $this->id = uniqid();
+        $this->message = $message;
+        print_r("Se creó un nuevo objeto Post \n");
+
+    }
+
+
 
     public function saludo() {
         return "Hola";
@@ -14,16 +27,18 @@ class Post{
         return $this->id;
     }
 
+    public function getMessage():string {
+        return $this->$message;
+    }
+
     public function setId(string $id) {
         $this->$id = $id;
     }
 
+    public function setMessage(string $message) {
+        $this->$message = $message;
+    }
 
 }
 
-$obj = new Post;
-
-// var_dump($obj);
-
-echo $obj->getId();
-echo $obj->saludo();
+$obj = new Post("Holaaaaaa");
